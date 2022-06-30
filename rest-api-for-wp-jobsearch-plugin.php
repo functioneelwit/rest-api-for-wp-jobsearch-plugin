d830b26233d237b646245b24eb487a155728722e
Mattijs Wit <mattijs@functioneelwit.nl>
2022-06-30 09:40:47 +0200

    Init

---
+++ rest-api-for-wp-jobsearch-plugin.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Plugin Name:       Rest API support for Jobsearch plugin
+ * Plugin URI:        https://functioneelwit.nl/
+ * Description:       Add WP REST API support for jobs in Jobsearch plugin.
+ * Version:           1.0.0
+ * Author:            Functioneelwit.nl
+ * Author URI:        https://functioneelwit.nl/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       rest-api-for-wp-jobsearch-plugin
+ */
+ 
+ /**
+ * This filter adds REST API support to an already registered
+ * job custom post type.
+ */
+
+ add_filter( 'register_post_type_args', 'job_post_type_args', 10, 2 );
+
+function job_post_type_args( $args, $post_type ) {
+    if ( 'job' === $post_type ) {
+        $args['show_in_rest'] = true;
+    }
+    return $args;
+}
+
+/**
+ * This adds all job meta data for use in WP REST API
+ */
+
+register_rest_field( 'job', 'meta', ['get_callback' => function ( $data ) {
+    return get_post_meta( $data['id'], '', '' );
+}]);
+
